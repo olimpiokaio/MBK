d2b3d7e23c7f1b73cdf14b26fb3b04de1f657eaf
Olimpio Kaio <olimpiocode@gmail.com>
2025-08-16 23:25:23 -0300

    refatorando component community

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PlayComponent } from './play/play.component';
 import { MatchComponent } from './match/match.component';
+import { CommunityComponent } from './community/community.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'play', component: PlayComponent },
+  { path: 'community', component: CommunityComponent },
   { path: 'match/:id', component: MatchComponent },
   { path: '**', redirectTo: '' }
 ];
--- src/app/community/community.component.ts
+++ src/app/community/community.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { CardCommunitComponent } from '../shared/card-communit/card-communit.component';
+import { BackButtonComponent } from '../shared/back-button/back-button.component';
+import { LoadingSpinnerComponent } from '../shared/loading-spinner/loading-spinner.component';
+import { CommunityService } from '../services/community.service';
+import { PlayersService } from '../services/players.service';
+import { Communitiy } from './community.model';
 
 @Component({
   selector: 'app-community',
   standalone: true,
-  imports: [],
+  imports: [CardCommunitComponent, BackButtonComponent, LoadingSpinnerComponent],
   templateUrl: './community.component.html',
   styleUrl: './community.component.css'
 })
 export class CommunityComponent {
+  private router = inject(Router);
+  private communityService = inject(CommunityService);
+  private playersService = inject(PlayersService);
 
+  comunitys: Communitiy[] = [];
+  loadingCommunities = true;
+
+  constructor() {
+    this.communityService.getCommunities().subscribe({
+      next: (list) => {
+        this.comunitys = list.map(c => ({ ...c }));
+        // Atualiza quantidade de membros conforme PlayersService
+        this.comunitys.forEach((c) => {
+          this.playersService.getPlayersByCommunity(c.id).subscribe(players => {
+            c.membersQuantity = players.length;
+          });
+        });
+        this.loadingCommunities = false;
+      },
+      error: () => {
+        this.loadingCommunities = false;
+      }
+    });
+  }
+
+  onSelected(communit: Communitiy) {
+    this.router.navigate(['/match', communit.id], { state: { name: communit.name } });
+  }
 }
