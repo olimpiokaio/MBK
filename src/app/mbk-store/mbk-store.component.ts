f4ffc53aa096ce338e7c12477d03665d1eb7bae0
Olimpio Kaio <olimpiocode@gmail.com>
2025-08-19 18:56:44 -0300

    adicionando mais gifs

--- src/app/mbk-store/mbk-store.component.ts
+++ src/app/mbk-store/mbk-store.component.ts
@@ -22,26 +22,40 @@ export class MbkStoreComponent implements OnDestroy {
   readonly STORAGE_KEY = 'mbk.store.purchased.backgrounds';
   readonly APPLIED_KEY_BASE = 'mbk.store.applied.background';
 
-  // Lista de GIFs disponíveis na pasta public/backgrond-modal
+  // Lista de GIFs disponíveis na pasta public/background-modal
   items: StoreItem[] = [
     'animal-jam.gif',
     'balling.gif',
+    'batman.gif',
     'breakin-bad-2.gif',
     'breaking-bad.gif',
+    'canes-cup.gif',
+    'crazy-traffic.gif',
     'daffy-duck-looney-tunes.gif',
+    'darth-vader-star-wars.gif',
+    'death-note-character.gif',
     'diadop.gif',
     'diana-taurasi.gif',
+    'elden-ring.gif',
     'emo.gif',
+    'goku.gif',
+    'gothic-joker.gif',
+    'itachi-uchiha.gif',
     'kira-yoshikage.gif',
     'legend-optimus-prime.gif',
+    'light-yagami-and-misa-amane.gif',
     'lola-bunny.gif',
     'male.gif',
+    'mei-misaki-another.gif',
     'nancysaey.gif',
     'ny.gif',
     'saul-goodman.gif',
+    'shaggy-scooby-doo.gif',
     'space-Jam-Monstars.gif',
+    'the-nyan-cat.gif',
     'trikey-drama.gif',
-  ].map((file) => ({ id: file, name: file.replace('.gif',''), src: `/backgrond-modal/${file}`, cost: this.COST }));
+    'wawa.gif',
+  ].map((file) => ({ id: file, name: file.replace('.gif',''), src: `/background-modal/${file}`, cost: this.COST }));
 
   private purchasedSet = new Set<string>(this.readPurchased());
 
